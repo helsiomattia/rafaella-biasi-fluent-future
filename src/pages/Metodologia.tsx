4ef76fa3e6cdcd219ea023dee0dd88e792c0879f
Helsio Mattia <103640564+helsiomattia@users.noreply.github.com>
2026-03-29 16:07:24 -0300

    Update Metodologia.tsx

--- src/pages/Metodologia.tsx
+++ src/pages/Metodologia.tsx
@@ -1,18 +1,16 @@
 import Layout from "@/components/layout/Layout";
 import MetodologiaHero from "@/components/metodologia/MetodologiaHero";
-import FilosofiaMetodologia from "@/components/metodologia/FilosofiaMetodologia";
-import MetodologiaSteps from "@/components/metodologia/MetodologiaSteps";
+import MetodologiaGrid from "@/components/metodologia/MetodologiaGrid";
+import ComoFuncionaMetodologia from "@/components/metodologia/ComoFuncionaMetodologia";
 import DiferenciaisMetodologia from "@/components/metodologia/DiferenciaisMetodologia";
-import ResultadosMetodologia from "@/components/metodologia/ResultadosMetodologia";
 import CtaFinalSection from "@/components/home/CtaFinalSection";
 
 const Metodologia = () => (
   <Layout>
     <MetodologiaHero />
-    <FilosofiaMetodologia />
-    <MetodologiaSteps />
+    <MetodologiaGrid />
+    <ComoFuncionaMetodologia />
     <DiferenciaisMetodologia />
-    <ResultadosMetodologia />
     <CtaFinalSection />
   </Layout>
 );
